8e04526c40c866a979e724ff1782df30ac0aee06
Churi <mail@churi.dev>
2026-07-09 11:42:59 +1200

    Setup glfw window

--- src/main.c
+++ src/main.c
@@ -1,6 +1,50 @@
+#include <glad/glad.h>
 #include <GLFW\glfw3.h>
 #include <stdio.h>
 
-int main() {
-    printf("Hello!");
+#
+
+int main()
+{
+    #ifndef NDEBUG
+    printf("You are Running a debug build.\n\n\n");
+    #endif
+
+    glfwInit();
+    // glfwWindowHint configures window options. Prams are (Option Enum, Value), Eg here sets the verison to 3.3
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Create the window
+    GLFWwindow* window = glfwCreateWindow(800, 600, "CGRA Engine", NULL, NULL);
+    if (window == NULL)
+    {
+        #ifndef NDEBUG
+        printf("Failed to create GLFW window\n");
+        #endif
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // Init GLAD
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+        #ifndef NDEBUG
+        printf("Failed to initialize GLAD\n");
+        #endif
+        return -1;
+    }
+
+    // Set the GL viewport
+    glViewport(0, 0, 800, 600);
+
+    // Main glfw loop
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    return 0;
 }
