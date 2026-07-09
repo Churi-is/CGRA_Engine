f49c53bae53e200fbf2197feb892b1d1a45dc5ad
Churi <mail@churi.dev>
2026-07-09 13:41:22 +1200

    Refactor main.c

--- src/main.c
+++ src/main.c
@@ -2,14 +2,12 @@
 #include <GLFW\glfw3.h>
 #include <stdio.h>
 
-#
+void exit_app(int code) {
+    glfwTerminate();
+    exit(code);
+}
 
-int main()
-{
-    #ifndef NDEBUG
-    printf("You are Running a debug build.\n\n\n");
-    #endif
-
+GLFWwindow* initialise_glfw_window() {
     glfwInit();
     // glfwWindowHint configures window options. Prams are (Option Enum, Value), Eg here sets the verison to 3.3
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -24,8 +22,7 @@ int main()
         #ifndef NDEBUG
         printf("Failed to create GLFW window\n");
         #endif
-        glfwTerminate();
-        return -1;
+        exit_app(-1);
     }
     glfwMakeContextCurrent(window);
 
@@ -34,20 +31,37 @@ int main()
         #ifndef NDEBUG
         printf("Failed to initialize GLAD\n");
         #endif
-        return -1;
+        exit_app(-1);
     }
 
     // Set the GL viewport
     glViewport(0, 0, 800, 600);
+    return window;
+}
+
+void process_input(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+int main()
+{
+    #ifndef NDEBUG
+    printf("You are Running a debug build.\n\n\n");
+    #endif
+
+    GLFWwindow* window = initialise_glfw_window();
 
     // Main glfw loop
-    while(!glfwWindowShouldClose(window))
-    {
+    while(!glfwWindowShouldClose(window)){
+        process_input(window);
+
+        glClearColor(0.5f, 0.5, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    glfwTerminate();
-
-    return 0;
+    exit_app(0);
 }
